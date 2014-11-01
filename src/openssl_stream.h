c3d4c6178f7acdc067442b041539cce7fb5fe96b
Carlos Martín Nieto <cmn@dwim.me>
2014-11-01 20:15:15 +0100

    fixup! Add an OpenSSL IO stream

--- src/openssl_stream.h
+++ src/openssl_stream.h
@@ -4,10 +4,10 @@
  * This file is part of libgit2, distributed under the GNU GPL v2 with
  * a Linking Exception. For full terms see the included COPYING file.
  */
-#ifndef INCLUDE_socket_stream_h__
-#define INCLUDE_socket_stream_h__
+#ifndef INCLUDE_openssl_stream_h__
+#define INCLUDE_openssl_stream_h__
 
-#include "git/sys/stream.h"
+#include "git2/sys/stream.h"
 
 extern int git_openssl_stream_new(git_stream **out, const char *host, const char *port);
 
